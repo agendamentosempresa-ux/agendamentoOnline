482350baadd1f6eb65bbaee949bd4cbcc16ed379
xxx <xxx@xxx.com>
2025-11-04 19:02:01 -0300

    Atualizações css projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
+import "./assets/Dir.css";
+/* Import do seu CSS externo */
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
 
 createRoot(document.getElementById("root")!).render(<App />);
